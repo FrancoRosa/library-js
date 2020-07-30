c01c4c5ddef607eefbeac1617c060618c38cb723
FrancoRosa <km115.franco@gmail.com>
2020-07-30 17:19:30 -0500

    Show array on html

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const bookList = [];
 function addBook(title, author, pages, arr) {
   let book = new Book(title, author, pages);
   arr.push(book);
+  showBooks(arr);
 }
 
 document.addEventListener("submit", function(event) {
@@ -18,5 +19,24 @@ document.addEventListener("submit", function(event) {
   let author = document.getElementById("book_author_id").value;
   let pages = document.getElementById("book_pages_id").value;
   addBook(title, author, pages, bookList);
-  alert("Book added");
 })
+
+function showBooks (arr){
+  
+  section = document.getElementById("books_container");
+  section.innerHTML = ''; 
+  arr.forEach((book,index) => {
+    div1 = document.createElement("div");
+    div1.setAttribute('data-attribute', index);
+    let p1 = document.createElement("p");
+    p1.innerHTML = book.title;
+    let p2 = document.createElement("p");
+    p2.innerHTML = book.author;
+    let p3 = document.createElement("p");
+    p3.innerHTML = book.pages;
+    div1.appendChild(p1);
+    div1.appendChild(p2);
+    div1.appendChild(p3);
+    section.appendChild(div1);
+  });
+}
